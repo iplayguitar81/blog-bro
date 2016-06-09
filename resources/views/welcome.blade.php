9706e37e4b0a64ee551aaf7d32621e01d4c60754
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-09 00:39:29 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,155 +1,17 @@
-
-
 @extends('layout')
-@section('title', 'Home')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Welcome</div>
 
-<div class="col-md-8">
-    @if(Session::has('message'))
-        <div class="alert alert-info" style="color:red;">
-            {{Session::get('message')}}
+                <div class="panel-body">
+                    Your Application's Landing Page.
+                </div>
+            </div>
         </div>
-    @endif
-
-    {{--{{'My sources tell me your location is '.$location['city'].','.$location['state']}}--}}
-
-
-    @foreach($posts as $item)
-    <article class="text-center">
-
-
-        <h1 class="main-article-titles" style="font-family: Pacifico, cursive;">
-            <a href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}">{{ $item->title }}</a>
-        </h1>
-        <p class="subheader-main" style="font-family: Boogaloo, cursive; font-size:2em;">{{ $item->subHead}}</p>
-        {{--{{$posts-> $item->user }}--}}
-
-        {{--{{//$users::where('id','like',$item->user_id) -> name()}}--}}
-        <p class="">Written by
-            <?
-            //below is one way to get the name of the author.....
-            ?>
-
-            @if($item->user_id != null)
-            <? $author = App\User::find($item->user_id)->name; ?>
-
-            {{$author}}
-            @endif
-            {{--@foreach($records as $record)--}}
-
-                {{--{{$record->name}}--}}
-                {{--@endforeach--}}
-
-            on {{ $item->created_at->format('M dS Y') }}</p>
-
-        <p>
-            <a href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}"><img class="img-responsive center-block" src="../images/{{ $item->imgPath}}"></a>
-        </p>
-
-            {{--{{//# way to declare variables in view next line......}}--}}
-        {{--*/ @ $rate_sum = 0; $rate_count=0; $rate_avg=0; $rate_pct=0;  /*--}}
-
-
-
-
-        @foreach($ratings as $rating)
-        @if($rating->post_id ==$item->id)
-
-     {{--*/ @ $rate_sum+=$rating->rating; $rate_count++; $rate_avg=$rate_sum/$rate_count; $rate_pct=($rate_avg/5)*100 /*--}}
-
-
-
-
-            {{--<p>{{$rating->rating}}</p>--}}
-            {{--<p>{{$rating->rate_message}}</p>--}}
-
-            @else
-
-
-            @endif
-
-
-        @endforeach
-
-
-        <br/>
-
-
-        {{--<div class="row">--}}
-            {{--<div class="col-md-8">--}}
-
-                {{--{{'Rating Avg: '.round($rate_avg,2)}}/5--}}
-                {{--<br/>--}}
-                {{--{{'# of Ratings: '.$rate_count}}--}}
-
-                {{--<div class="rating center-block"><div class="stars"></div><div class="back" style="width:{{$rate_pct}}%;"></div></div>--}}
-
-
-            {{--</div>--}}
-
-        {{--</div>--}}
-
-        <br/>
-
-<!--            --><?//$ratings= $ratings($item->id)?>
-
-        {{--<p>Average Rating: {{$ratings->averageRating}}</p>--}}
-        {{--<p>Rating %: {{$ratings->ratingPercent}}</p>--}}
-
-{{--{{$variable = str_limit($item->body, 100)}}--}}
-   <?
-
-            $variable= strip_tags($item->body);
-            $variable =substr($variable,0, 50);
-       // $variable = (str_limit($item->body, 100));
-       // $variable= htmlentities($variable);
-        ?>
-       <p class="article-texterson">{{$variable}} ...</p>
-      {{--<p>  {{strip_tags((str_limit($item->body, 100)))}}...</p>--}}
-        <br/>
-            <a class="btn btn-danger btn-md active" href="{{ route('posts.show', [$item->id, str_slug($item->title)]) }}">Continue Reading</a>
-            <a style="margin-top:.2em;" class="btn btn-primary btn-md active" href="#">Comments</a>
-        <hr>
-
-    </article>
-
-    @endforeach
-
-        {{--<h1>All Cards</h1>--}}
-
-
-
-        {{--<!-- Project One -->--}}
-        {{--<div class="row">--}}
-            {{--<div class="col-md-7">--}}
-                {{--<a href="#">--}}
-                    {{--<img class="img-responsive" src="http://placehold.it/700x300" alt="">--}}
-                {{--</a>--}}
-            {{--</div>--}}
-            {{--<div class="col-md-5">--}}
-                {{--<h3><a href="/cards/1">once upon a rhyme</a></h3>--}}
-                {{--<h4>2016-04-28 00:00:00</h4>--}}
-                {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>--}}
-                {{--<a class="btn btn-primary" href="/cards/1">View Card <span class="glyphicon glyphicon-chevron-right"></span></a>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-
-<br/>
-        <br/>
-        <div class="pagination"> {!! $posts->render() !!} </div>
-
-
+    </div>
 </div>
-<br/>
-
-<br/>
-
-<div class="col-md-3 pull-right">
-    @include('sidebar')
-</div>
-
-
 @endsection
-
-
