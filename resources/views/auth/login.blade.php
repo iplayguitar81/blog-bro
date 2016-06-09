2b3f2833009bf5eeaeb02acd2a555849fe7851a9
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-09 00:45:00 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,13 +8,13 @@
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {!! csrf_field() !!}
+                        {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -25,10 +25,10 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
+                                <input id="password" type="password" class="form-control" name="password">
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -51,22 +51,13 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
+                                    <i class="fa fa-btn fa-sign-in"></i> Login
                                 </button>
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                                {{--<a class="btn btn-primary" href="{{ route('social.login', ['facebook']) }}">Facebook</a>--}}
                             </div>
                         </div>
                     </form>
-            <h1>Login/Register with your favorite Social Media:</h1>
-<p>This can be convenient for those who do not wish to keep track of remembering another username and password just for use on this site.  The alternative to that is to login with your favorite social media provider below.  We will only store your email address and name from these sites.  We will not sell your information to anyone!  Thank you!  This was implemented as a convenience to you! :D</p>
-                    <div class="list-group">
-                        <a href="{{url('facebook/authorize')}}"><button type="button" class="list-group-item"><i class="fa fa-lg fa-facebook"></i> &nbsp;Facebook</button></a>
-                        <a href="{{url('google/authorize')}}"><button type="button" class="list-group-item"><i class="fa fa-lg fa-google"></i> &nbsp;Google</button></a>
-                        <a href="{{url('github/authorize')}}"><button type="button" class="list-group-item"><i class="fa fa-lg fa-github"></i> &nbsp;Github</button></a>
-
-                    </div>
                 </div>
             </div>
         </div>
