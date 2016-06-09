728457e498970d3c5f3e0e031cfeff66f3b5f1b9
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-09 02:14:21 -0700

    get win 10 laravel started yeahhhhhh

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -63,7 +63,7 @@
             {{--*/ @ $rate_pct_reviewer = (($rating->rating/5)*100); /*--}}
 
 
-                @if($rating->user_id== Auth::user()->id || $post->user_id== Auth::user()->id)
+                @if( $post->user_id== Auth::user()->id)
                     {{--*/ @ $hide_rating_form = true; /*--}}
                 @endif
 
