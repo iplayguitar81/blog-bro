63bd73b18d3bdd8c7d9b8fd89b77b34d9eedd202
Chris M Snyder <iplayguitar81@yahoo.com>
2016-06-09 15:56:13 -0700

    get win 10 laravel started yeahhhhhh

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,13 +7,9 @@ use App\Http\Controllers\Controller;
 use Gate;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Facade;
-
 use App\Post;
-
 use \App;
 use Illuminate\Support\Facades\DB;
-
-
 use App\User;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -328,7 +324,7 @@ class PostsController extends Controller
         $posts = Post::where('user_id','=', Auth::id())->orderBy('created_at', 'desc')->get();
 
 //export all posts for super user
-//        $posts = Post::select('user_id', 'title', 'subhead','body','imgpath', 'created_at')->get();
+//$posts = Post::select('user_id', 'title', 'subhead','body','imgpath', 'created_at')->get();
 
         Excel::create('blog-posts', function($excel) use($posts) {
             $excel->sheet('Sheet 1', function($sheet) use($posts) {
